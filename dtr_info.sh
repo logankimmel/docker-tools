c965c5d8de3a54b3971543eec5bba65106e64fb0
Logan Kimmel <logan.kimmel@docker.com>
2019-10-23 11:41:56 -0400

    max pagecount

--- dtr_info.sh
+++ dtr_info.sh
@@ -3,7 +3,7 @@ read DTR_HOSTNAME
 echo -n "Enter your token and press [ENTER]: "
 read TOKEN
 
-repos=$(curl -s -u admin:$TOKEN -X GET "https://$DTR_HOSTNAME/api/v0/repositories?pageSize=0&count=true" -H "accept: application/json" | jq -r -c .repositories)
+repos=$(curl -s -u admin:$TOKEN -X GET "https://$DTR_HOSTNAME/api/v0/repositories?pageSize=100000&count=true" -H "accept: application/json" | jq -r -c .repositories)
 echo "Repo Count: $(echo $repos | jq 'length')"
 repo_list=$(echo "${repos}" | jq -c -r '.[]')
 # # Loop through repos to get total tags
